0cf2ad5cc01b5c161b354eb5cf16241cbba9a719
matthyk <matthias.keckl.develop@gmail.com>
2021-05-01 12:56:01 +0200

    use GetRouteDefinition for get collection decorator

--- src/router/http-methods/get/get.collection-decorator.ts
+++ src/router/http-methods/get/get.collection-decorator.ts
@@ -1,18 +1,8 @@
 import { Route } from '../../decorators'
 import { AbstractModel } from '../../../models'
 import { AbstractGetCollectionState } from '../../../api'
-import { ViewConverter } from '../../route-definitions'
 import { ReturnsConfiguredState, TypedMethodDecorator } from '../../types'
-
-/**
- * There should be no validation schema defined for GET requests, because the request payload should be ignored anyway.
- * See https://tools.ietf.org/html/rfc7231#section-4.3.1
- */
-export interface GetRouteDefinition {
-  path?: string
-  viewConverter: ViewConverter
-  produces: string
-}
+import { GetRouteDefinition } from './get.decorator'
 
 export const GetCollection = <
   State extends AbstractGetCollectionState<AbstractModel>
